3915ff6ba1b9a83bd410267d3884a4ddae6ee221
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-11-30 19:28:06 +0500

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Arkasha Zuev on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
